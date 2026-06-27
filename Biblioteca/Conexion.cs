5f216bd23cc31911e8d097e71cdb518e257b7562
JhocepGab <snkje123@gmail.com>
2026-06-27 12:19:46 -0500

    Añade clase Conexion y la incluye en el proyecto
    Se crea el archivo Conexion.cs con la clase Conexion, que representa una arista del grafo con los atributos destino y linea. Se actualiza Biblioteca.csproj para incluir este nuevo archivo en la compilación.

---
+++ Biblioteca/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Conexion
+    {
+        //aristas del grafo
+        public Estacion destino;
+        public string linea;
+    }
+}
